8a049f3ab0dca10d0914adc7e5b1850be6e2a2e6
Paolo Zacchi <zacchi.paolo@gmail.com>
2024-02-23 09:38:04 +0100

    First commit

---
+++ Tests.cs
@@ -0,0 +1,20 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NUnit.Framework;
+
+namespace EDataTest
+{
+	public class Tests
+	{
+		private readonly string _path = AppDomain.CurrentDomain.BaseDirectory + @"\";
+
+		[Test]
+		public void TestEDataFunction()
+		{
+			using var fileStream = new FileStream(_path + "EDATEBug.xls", FileMode.Open, FileAccess.Read, FileShare.Read);
+			IWorkbook workbook = new HSSFWorkbook(fileStream, true);
+			HSSFFormulaEvaluator.EvaluateAllFormulaCells(workbook);
+		}
+
+	}
+}
